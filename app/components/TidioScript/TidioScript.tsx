4925a33633ceec20184d583337a4bdaa4cbfdb81
LittleGiddy <gideonfelixy@gmail.com>
2025-05-15 19:28:26 +0300

    Add Tidio AI assistant

---
+++ app/components/TidioScript/TidioScript.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function TidioScript() {
+  return (
+    <Script src="//code.tidio.co/ciwnugokeyrqyjbuytxq3obuxpsck58k.js" async/>
+  );
+}
