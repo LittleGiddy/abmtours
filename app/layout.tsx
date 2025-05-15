f143335f64981dbfa622a9186dbb1bc59ea5daf0
LittleGiddy <154503514+LittleGiddy@users.noreply.github.com>
2025-05-15 17:21:23 +0300

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./components/ClientLayout"; // Import the client component
-import TidioScript from "./components/TidioScript";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,7 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ colorScheme: "light" }}>
       <body className={${font.className} antialiased flex flex-col min-h-screen bg-white text-black}>
-        <script src="//code.tidio.co/ciwnugokeyrqyjbuytxq3obuxpsck58k.js" async></script>
         <ClientLayout>{children}</ClientLayout> {/* Use the client component here */}
       </body>
     </html>
