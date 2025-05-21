35d60543e2f434133804f30b15fa615c90996be8
LittleGiddy <154503514+LittleGiddy@users.noreply.github.com>
2025-05-21 10:56:24 +0300

    WhyUsCard.tsx

--- app/components/Home/WhyUs/WhyUsCard.tsx
+++ app/components/Home/WhyUs/WhyUsCard.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Image from "next/image";
-
+import { motion } from "framer-motion";
 
 type Props = {
   image: string;
   title: string;
   description: string;
 };
+
 const WhyUsCard = ({ image, title, description }: Props) => {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.3 }} // triggers when 30% of card is in view
+    >
       <Image
         src={image}
         width={90}
@@ -17,14 +23,13 @@ const WhyUsCard = ({ image, title, description }: Props) => {
         alt="image"
         className="mx-auto"
       />
-      {/* content */}
-      <h1 className="mt-6 text-center  text-gray-900 font-medium text-xl">
+      <h1 className="mt-6 text-center text-gray-900 font-medium text-xl">
         {title}
       </h1>
       <p className="mt-2 text-center text-s font-medium text-gray-400">
-       {description}
+        {description}
       </p>
-    </div>
+    </motion.div>
   );
 };
 
