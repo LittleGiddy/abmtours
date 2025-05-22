647d8c834c61ec1cf4e3d3b51ece31e8cbeb0846
LittleGiddy <154503514+LittleGiddy@users.noreply.github.com>
2025-05-22 10:46:18 +0300

    TidioScript.tsx

--- app/components/TidioScript/TidioScript.tsx
+++ app/components/TidioScript/TidioScript.tsx
@@ -1,10 +1,20 @@
 'use client';
 
 import Script from 'next/script';
-
+import { usePathname } from 'next/navigation';
 
 export default function TidioScript() {
+  const pathname = usePathname();
+
+  // Prevent Tidio from loading on /admin or any sub-routes like /admin/dashboard
+  const isAdminRoute = pathname.startsWith('/admin');
+
+  if (isAdminRoute) return null;
+
   return (
-    <Script src="//code.tidio.co/ciwnugokeyrqyjbuytxq3obuxpsck58k.js" async/>
+    <Script
+      src="//code.tidio.co/ciwnugokeyrqyjbuytxq3obuxpsck58k.js"
+      strategy="afterInteractive"
+    />
   );
 }
