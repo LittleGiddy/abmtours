b37ce2ff12fe7b2c1bd3ded16a180db27c9b5a58
LittleGiddy <154503514+LittleGiddy@users.noreply.github.com>
2025-06-20 01:42:40 +0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-//import Home from './components/Home/Home'
+import Home from './components/Home/Home'
 
 
 
 
 const HomePage = () => {
   return (
-   <div style={{ textAlign: 'center', padding: '100px 20px' }}>
-      <h1>Something went wrong!</h1>
-      <p>Refresh or try again later. If the problem persists, contact your host provider</p>
-    </div>
+      < Home />
   )
 }
  
